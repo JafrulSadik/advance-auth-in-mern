f7d28dbc947528107d6e99a9ad4a7d8bd3e19ab1
JafrulSadik <jafrul.sadik4@gmail.com>
2024-09-18 00:54:05 +0600

    chore: initialize React app and set up Tailwind CSS configuration

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-green-950 via-green-600 to-slate-950">
+      Hello!
+    </div>
+  );
+}
+
+export default App;
