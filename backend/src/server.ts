8bb76f550b66ea0a7d71df5eee500accfdc99690
JafrulSadik <jafrul.sadik4@gmail.com>
2024-10-01 22:47:41 +0600

    chore: add CORS package for cross-origin requests and intregate in app

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 import config from "./config/config";
 import { db } from "./config/db";
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 
