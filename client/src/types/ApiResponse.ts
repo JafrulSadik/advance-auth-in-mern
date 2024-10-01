65308cc6aed37d052c39449d504f403fae2e8569
JafrulSadik <jafrul.sadik4@gmail.com>
2024-10-01 22:46:35 +0600

    feat: add error response handling

--- client/src/types/ApiResponse.ts
+++ client/src/types/ApiResponse.ts
@@ -1,5 +1,12 @@
 export type ApiSuccessfullResponse<TData> = {
   code: number;
+  success: boolean;
   message: string;
   data: TData;
+};
+
+export type ApiErrorResponse = {
+  success: boolean;
+  code: number;
+  message: string;
 };
